54e579640969a82feb8e117a61c960285bff25a1
Michael Vorburger <mike@vorburger.ch>
2013-09-15 19:14:06 +0200

    Parameterized Test name (Browser Type) read from WebDriverProvider now!

--- webdriver-runner-core/src/test/java/ch/vorburger/webdriver/runner/core/examples/WebDriverParametrizedExampleTest.java
+++ webdriver-runner-core/src/test/java/ch/vorburger/webdriver/runner/core/examples/WebDriverParametrizedExampleTest.java
@@ -3,7 +3,9 @@ package ch.vorburger.webdriver.runner.core.examples;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,29 +20,38 @@ import ch.vorburger.webdriver.runner.core.WebDriverProvider;
 import ch.vorburger.webdriver.runner.core.providers.ChromeDriverProvider;
 import ch.vorburger.webdriver.runner.core.providers.FirefoxDriverProvider;
 
-@RunWith(Parameterized.class)
+@RunWith(Parameterized.class) // @see https://github.com/junit-team/junit/wiki/Parameterized-tests
 public class WebDriverParametrizedExampleTest {
 	
-	// TODO refactor push up into and use AbstractWebDriverTest
-	
-	// TODO test name!!
+	// TODO refactor push up into and use AbstractWebDriverTest (or, better, a composed helper...)
 	
 	// Field injected @Parameter preferable over Constructor injection, because Constructor with super would have to be repeated in each test class  
 	// NOTE: Until (if) https://github.com/junit-team/junit/pull/737 makes it in, this HAS to be public (cannot be protected)
-	@Parameter public WebDriverProvider webDriverProvider;
+	@Parameter(value=0) public String webDriverProviderName;
+	@Parameter(value=1) public WebDriverProvider webDriverProvider;
 	protected WebDriver w;
 
 	@Before public void beforeTest() throws Exception {
 		w = webDriverProvider.getNewWebDriver();
 	}
 	
-	@Parameters public static Iterable<WebDriverProvider[]> webDrivers() {
-		// TODO use/delegate to MyWebDriverTestConfiguration idea instead?
-		// TODO switch this to use the new RecyclingDriverProvider.. keep the entire config. outside.. how/where?
-		return Arrays.asList(new WebDriverProvider[][] {
-				new WebDriverProvider[] { new ChromeDriverProvider() }, 
-				new WebDriverProvider[] { new FirefoxDriverProvider() } 
+	// TODO keep the entire config. outside.. how/where?
+	// TODO use/delegate to MyWebDriverTestConfiguration idea instead?
+	@Parameters(name = "{0}") 
+	public static Iterable<Object[]> webDriverProvidersAndName() {
+		// TODO switch this to use the new RecyclingDriverProvider.. 
+		Iterable<WebDriverProvider> providers = Arrays.asList( new WebDriverProvider[] {
+			new ChromeDriverProvider(), new FirefoxDriverProvider()
 		});
+		return getAsParameters(providers);
+	}
+	protected static Iterable<Object[]> getAsParameters(Iterable<WebDriverProvider> providers) {
+		// TODO use Google thing to transform.. 
+		Collection<Object[]> parameters = new ArrayList<Object[]>();
+		for (WebDriverProvider provider : providers) {
+			parameters.add(new Object[] { provider.getName(), provider  } );
+		}
+		return parameters;
 	}
 	
 	@Test public void testGoogle() {
